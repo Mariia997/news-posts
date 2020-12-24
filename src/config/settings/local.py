47792a2548df6237c82985d0a1220d0b5b879d94
Mariia Muliarchuk <mariyamul997@gmail.com>
2020-12-24 14:56:32 +0200

    posts models

---
+++ src/config/settings/local.py
@@ -0,0 +1,4 @@
+from .base import *
+
+
+SECRET_KEY = 'xgu&uq0a@vyx^j6c(k@e_bf2ouql&y*pmo3o!sa4cn3$8%a$k&'
---
+++ src/posts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    upvotes = models.PositiveIntegerField()
+    author_name = models.CharField(max_length=200)
+    link = models.URLField(max_length=2000)
+
+    class Meta:
+        db_table = "posts"
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=200)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "comments"
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return f"Comment {self.pk}"
+
+
+
